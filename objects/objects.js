eeef76470997a16a3ea201fb46bdb52f1228c246
Ornela Dodaj <orneladodaj.work@gmail.com>
2025-02-10 14:32:14 +0100

    learned about objects

---
+++ objects/objects.js
@@ -0,0 +1,23 @@
+const person ={
+    name: "Leo",
+    age:1,
+    height:70,
+    weight:10,
+};
+
+const userInputValues = ["age", "name", "height"];
+let userInput = userInputValues[1];
+
+
+console.log(person[userInput]); // console.log(person["name"]);
+
+
+
+// console.log(person);
+// console.log("Person's age: " +person.age +person.name);
+// console.log("Person's age: ", person.age , person.name);
+
+
+
+// var query = "age";
+// console.log("Person's age bracket: ", person[query] );
