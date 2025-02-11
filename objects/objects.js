de56a53d8fe891863a72448ea33a123439fc763a
Ornela Dodaj <orneladodaj.work@gmail.com>
2025-02-11 15:45:50 +0100

    created some objects

--- objects/objects.js
+++ objects/objects.js
@@ -1,4 +1,4 @@
-const person ={
+ /*const person ={
     name: "Leo",
     age:1,
     height:70,
@@ -21,3 +21,25 @@ console.log(person[userInput]); // console.log(person["name"]);
 
 // var query = "age";
 // console.log("Person's age bracket: ", person[query] );
+*/
+// Create an object
+const car = {
+    type : "BMW",
+    colour : "black",
+    numberOfDoors : 4,
+    speed : 0,
+    move : function(speed){
+       this.speed = speed;
+       console.log("The car is moving at " +speed + "km/h.")
+    }
+}
+console.log(car);
+car.move(80);
+
+//Another object
+const house = {
+    colour : "white",
+    numberOfRooms : 5,
+    area : 100,
+    
+}
